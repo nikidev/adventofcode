4f49d04ec7783cdce16470045f69645639bb7de5
nikidev <nikolay.mikov1@gmail.com>
2017-04-09 20:29:27 +0300

    rewrited

--- day1/day1a.php
+++ day1/day1a.php
@@ -8,18 +8,33 @@
 
 $input = 'L5, R1, R3, L4, R3, R1, L3, L2, R3, L5, L1, L2, R5, L1, R5, R1, L4, R1, R3, L4, L1, R2, R5, R3, R1, R1, L1, R1, L1, L2, L1, R2, L5, L188, L4, R1, R4, L3, R47, R1, L1, R77, R5, L2, R1, L2, R4, L5, L1, R3, R187, L4, L3, L3, R2, L3, L5, L4, L4, R1, R5, L4, L3, L3, L3, L2, L5, R1, L2, R5, L3, L4, R4, L5, R3, R4, L2, L1, L4, R1, L3, R1, R3, L2, R1, R4, R5, L3, R5, R3, L3, R4, L2, L5, L1, L1, R3, R1, L4, R3, R3, L2, R5, R4, R1, R3, L4, R3, R3, L2, L4, L5, R1, L4, L5, R4, L2, L1, L3, L3, L5, R3, L4, L3, R5, R4, R2, L4, R2, R3, L3, R4, L1, L3, R2, R1, R5, L4, L5, L5, R4, L5, L2, L4, R4, R4, R1, L3, L2, L4, R3';
 
+//$input = 'R5, L5, R5, R3';
+
+
 $x = 0;
 $y = 0;
 $heading = 0;
 
+//define('NORTH',0);
+//define('EAST',1);
+//define('SOUTH',2);
+//define('WEST',3);
+
+const NORTH = 0;
+const EAST = 1;
+const SOUTH = 2;
+const WEST = 3;
+
 $steps = explode(', ',$input);
+
+
 
 foreach ($steps as $step)
 {
     $letter = $step[0];
     $distance = (int) substr($step,1);
 
-//    var_dump($heading);
+
 
     if ($letter === 'L')
     {
@@ -38,19 +53,19 @@ foreach ($steps as $step)
         $heading %= 4;
     }
 
-    if ($heading === 0)
+    if ($heading === NORTH)
     {
         $y+= $distance;
     }
-    elseif ($heading === 1)
+    elseif ($heading === EAST)
     {
         $x += $distance;
     }
-    elseif ($heading === 2)
+    elseif ($heading === SOUTH)
     {
         $y -= $distance;
     }
-    elseif ($heading === 3)
+    elseif ($heading === WEST)
     {
         $x -= $distance;
     }
